e5012d8ec65f8b845af3b60614c2e9da4c671dd4
GypsyRobot <GypsyRobot@users.noreply.github.com>
2021-08-26 01:07:15 +0000

    deploy: 7e7fce0798a7a4aaa52be009699f58c5632d15ce

---
+++ precache-manifest.6355bfe9dfea93dab934e0317f934b3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761bd7d8e0d41c473118a43ae9eeafb2",
+    "url": "https://gypsyrobot.github.io/reactjs-task-manager/index.html"
+  },
+  {
+    "revision": "8ac70d15dce553e1bb71",
+    "url": "https://gypsyrobot.github.io/reactjs-task-manager/static/css/main.01e58550.chunk.css"
+  },
+  {
+    "revision": "3f3202fb6ec2b581fb80",
+    "url": "https://gypsyrobot.github.io/reactjs-task-manager/static/js/2.6bcc59eb.chunk.js"
+  },
+  {
+    "revision": "8ac70d15dce553e1bb71",
+    "url": "https://gypsyrobot.github.io/reactjs-task-manager/static/js/main.79078eb6.chunk.js"
+  },
+  {
+    "revision": "59b8106fd0b7d0750713",
+    "url": "https://gypsyrobot.github.io/reactjs-task-manager/static/js/runtime~main.29ace4c0.js"
+  }
+]);
